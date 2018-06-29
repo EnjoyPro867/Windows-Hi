05ac71785a88380f080416496df12ebf106ff9a4
EnjoyPro867 <40395797+EnjoyPro867@users.noreply.github.com>
2018-06-29 14:40:14 +0300

    bot.cpp
    The main file with comments. Added welcome and signature. For the convenience of development, comments have been added, the rest can be read in them.

---
+++ bot.cpp
@@ -0,0 +1,26 @@
+// bot.cpp : Defines the entry point for the console application.
+//
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+// * Early Access Program                                *
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+// * GitHub - https://github.com/EnjoyPro867/Windows-Hi  *
+// * Windows Hi by EnjoyPro867                           *
+// * https://vk.com/enjoypro867                          *
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+
+#include "stdafx.h" 
+#include <iostream>
+using namespace std;
+int main()
+{
+
+// The main greeting
+// "true" - Enabled
+// "false" - Disabled
+	if (true){
+		cout << "Hello user, I'm Windows, I'm friends with people!\n";
+	}
+  // On the "return" to debug (MANDATORY)
+    return 0;
+}
